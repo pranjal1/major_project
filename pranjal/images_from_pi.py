1466c2fdaf77de049a4a8d7bd0f9c6137917303c
pranjal1 <pranjal750@gmail.com>
2017-02-24 18:25:57 +0545

    image transmission from pi to server ok

--- pranjal/images_from_pi.py
+++ pranjal/images_from_pi.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python
 #password is not needed as we are using private/public key for file transferring
-try:
-	import sys
-	import os
-except:
-	sys.exit(1)
+import sys
+import os
+import picamera
+import time
 
-try:
-	os.system("scp -r /home/pi/b_pthr.jpg pranjal@192.168.0.110:/home/pranjal/Desktop/images_pi")		
-except:
-	print "oops error"
-	sys.exit(1)
+
+FRAMES = 1000
+TIMEBETWEEN = 6
+
+frameCount = 0
+
+while frameCount < FRAMES:
+    imageNumber = str(frameCount).zfill(7)
+    try:
+    	os.system("raspistill -w 640 -h 480 -hf -o /home/pi/Desktop/images_from_camera/image%s.jpg"%(imageNumber))
+    except:
+    	print "Error in image capture!"
+    	sys.exit(1)
+
+    frameCount += 1
+    time.sleep(TIMEBETWEEN - 6) #Takes roughly 6 seconds to take a picture
+
+
+#transferring files from raspberry pi to server(my pc in this case) using scp
+number_of_images = FRAMES
+num_count =0
+WAIT_FOR_SCP_TRANSMIT = 6
+while num_count < number_of_images:
+	imageNumber = str(num_count).zfill(7)
+	try:
+		os.system("scp -r /home/pi/Desktop/images_from_camera/image%s.jpg pranjal@192.168.0.110:/home/pranjal/Desktop/images_pi"%(imageNumber))
+		time.sleep(WAIT_FOR_SCP_TRANSMIT - 3) #Taking roughly 3 seconds to transmit image		
+	except:
+		print "Error in scp file transmission"
+		sys.exit(1)
+		num_count+=1
+
