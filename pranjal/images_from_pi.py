70647250a0904fcdaed55c44f43fd656777dcd53
pranjal1 <pranjal750@gmail.com>
2017-02-24 18:44:48 +0545

    changed to using picamera instead of raspistill

--- pranjal/images_from_pi.py
+++ pranjal/images_from_pi.py
@@ -6,15 +6,19 @@ import picamera
 import time
 
 
+#image capture
 FRAMES = 1000
 TIMEBETWEEN = 6
 
 frameCount = 0
 
+camera = picamera.PiCamera()
+camera.resolution = (640,480)
+
 while frameCount < FRAMES:
     imageNumber = str(frameCount).zfill(7)
     try:
-    	os.system("raspistill -w 640 -h 480 -hf -o /home/pi/Desktop/images_from_camera/image%s.jpg"%(imageNumber))
+    	camera.capture('/home/pi/Desktop/images_from_camera/image%s.jpg'%(imageNumber))
     except:
     	print "Error in image capture!"
     	sys.exit(1)
@@ -23,17 +27,17 @@ while frameCount < FRAMES:
     time.sleep(TIMEBETWEEN - 6) #Takes roughly 6 seconds to take a picture
 
 
-#transferring files from raspberry pi to server(my pc in this case) using scp
-number_of_images = FRAMES
+#transferring captured images from raspberry pi to server(my pc in this case)
+number_of_images = 5
 num_count =0
 WAIT_FOR_SCP_TRANSMIT = 6
 while num_count < number_of_images:
 	imageNumber = str(num_count).zfill(7)
 	try:
-		os.system("scp -r /home/pi/Desktop/images_from_camera/image%s.jpg pranjal@192.168.0.110:/home/pranjal/Desktop/images_pi"%(imageNumber))
+		os.system("scp vvv -r /home/pi/Desktop/images_from_camera/image%s.jpg pranjal@192.168.0.110:/home/pranjal/Desktop/images_pi"%(imageNumber))
 		time.sleep(WAIT_FOR_SCP_TRANSMIT - 3) #Taking roughly 3 seconds to transmit image		
 	except:
 		print "Error in scp file transmission"
 		sys.exit(1)
-		num_count+=1
+	num_count+=1
 
