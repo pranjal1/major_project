47a0cead252bbd9bd25f65287a0b98145170f49d
pranjal1 <pranjal750@gmail.com>
2017-06-29 20:45:54 +0545

    fully connected layer

--- pranjal/spiral_data_NN.py
+++ pranjal/spiral_data_NN.py
@@ -32,29 +32,32 @@ reg = 1e-3 # regularization strength,lambda
 
 # gradient descent loop
 num_examples = X.shape[0]
-for i in xrange(10000):
+for i in xrange(10000):#10000
   
   # evaluate class scores, [N x K]
-  hidden_layer = np.maximum(0, np.dot(X, W) + b) # note, ReLU activation
-  scores = np.dot(hidden_layer, W2) + b2
+  hidden_layer = np.maximum(0, np.dot(X, W) + b) # note, ReLU activation, 300x100
+  scores = np.dot(hidden_layer, W2) + b2 #in every row, prediction for classes are stored for each input
   
   # compute the class probabilities
   exp_scores = np.exp(scores)
-  probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True) # [N x K]
+  probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True) # [N x K] # row = row/sum_row
   
   # compute the loss: average cross-entropy loss and regularization
-  corect_logprobs = -np.log(probs[range(num_examples),y])
-  data_loss = np.sum(corect_logprobs)/num_examples
-  reg_loss = 0.5*reg*np.sum(W*W) + 0.5*reg*np.sum(W2*W2)
+  corect_logprobs = -np.log(probs[range(num_examples),y])#taking row corresponding to right class and performing cross entropy, kada logic
+  data_loss = np.sum(corect_logprobs)/num_examples#cross entropy loss ko formula bata ako
+  reg_loss = 0.5*reg*np.sum(W*W) + 0.5*reg*np.sum(W2*W2)#punishing W by square, concept of regularization
   loss = data_loss + reg_loss
   if i % 1000 == 0:
     print "iteration %d: loss %f" % (i, loss)
   
-  # compute the gradient on scores
+  # compute the gradient on scores,how??????
   dscores = probs
-  dscores[range(num_examples),y] -= 1
+  dscores[range(num_examples),y] -= 1 #easy derivation
   dscores /= num_examples
   
+
+
+
   # backpropate the gradient to the parameters
   # first backprop into parameters W2 and b2
   dW2 = np.dot(hidden_layer.T, dscores)
@@ -62,7 +65,7 @@ for i in xrange(10000):
   # next backprop into hidden layer
   dhidden = np.dot(dscores, W2.T)
   # backprop the ReLU non-linearity
-  dhidden[hidden_layer <= 0] = 0
+  dhidden[hidden_layer <= 0] = 0#kada
   # finally into W,b
   dW = np.dot(X.T, dhidden)
   db = np.sum(dhidden, axis=0, keepdims=True)
